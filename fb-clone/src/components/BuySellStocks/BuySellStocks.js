64433c365ad68ed81195045238a3d7fbaf81072d
Robert Kim <robert23kim@gmail.com>
2021-12-10 23:27:41 -0500

    working, but still need to fix CORS

--- fb-clone/src/components/BuySellStocks/BuySellStocks.js
+++ fb-clone/src/components/BuySellStocks/BuySellStocks.js
@@ -11,15 +11,18 @@ const BuySellStocks = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(items);
-        buyStock();
-        event.preventDefault();
+        if(document.getElementById('transx_buy').checked) {
+            buyStock();
+        }else if(document.getElementById('transx_sell').checked) {
+            sellStock();
+        }
         const log = document.getElementById('log');
         log.textContent = `Purchase order submitted`;
     };
 
     const buyStock = async() => {
-        var url = 'https://d2kjnw8vmxc1wq.cloudfront.net/api/portfolio/api/buy/' + localStorage.getItem('user_id');
+        getStockPrice();
+        var url = 'https://d2kjnw8vmxc1wq.cloudfront.net/api/transaction/v3/buy/' + localStorage.getItem('user_id');
         var xhr = new XMLHttpRequest();
         xhr.open('POST', url, true);
 
@@ -32,36 +35,83 @@ const BuySellStocks = () => {
                 console.log(xhr.responseText);
             }
         };
-        var data = JSON.stringify({"ticker": items["ticker"], "quantity": items["quantity"]});
+        var data = JSON.stringify({
+                                            "ticker": items["ticker"],
+                                            "quantity": items["quantity"],
+                                            "price": items["price"]
+        });
         xhr.send(data);
+    }
+
+    const sellStock = async() => {
+        getStockPrice();
+        var url = 'https://d2kjnw8vmxc1wq.cloudfront.net/api/transaction/v3/buy/' + localStorage.getItem('user_id');
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', url, true);
+
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                console.log(xhr.status);
+                console.log(xhr.responseText);
+            }
+        };
+        var data = JSON.stringify({
+            "ticker": items["ticker"],
+            "quantity": items["quantity"],
+            "price": items["price"]
+        });
+        xhr.send(data);
+    }
+
+    const getStockPrice = async() => {
+        const data = await fetch(
+            'http://127.0.0.1:5000/api/stocks/' + items["ticker"]
+            //'https://d2kjnw8vmxc1wq.cloudfront.net/api/stocks/' + items["ticker"]
+        ).then(data => data.json());
+        console.log(data);
+        console.log(data.latest_price);
+        items["price"] = data.latest_price;
     }
 
     return (
         <div>
-            <p>Stock Transactions:</p>
+            <p>Transaction Type</p>
             <form onSubmit={handleSubmit}>
-                <input type="radio" id="buy" name="transaction" value="buy" checked></input>
-                <label for="buy">Buy </label>
-                <input type="radio" id="sell" name="transaction" value="sell"></input>
-                <label for="sell">Sell </label>
-                <label>Enter ticker:
-                    <input
-                        type="text"
-                        name="ticker"
-                        value={items.ticker || ""}
-                        onChange={handleChange}
-                    />
-                </label>
-                <label>Enter quantity:
-                    <input
-                        type="number"
-                        name="quantity"
-                        value={items.quantity || ""}
-                        onChange={handleChange}
-                    />
-                </label>
-                <input type="submit" />
-                <p id="log"></p>
+                <div>
+                    <label>Buy
+                        <input
+                            type="radio" name="transaction" id="transx_buy" value="Buy"
+                        />
+                    </label>
+                    <label>Sell
+                        <input
+                            type="radio" name="transaction" id="transx_sell" value="Sell"
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>Enter ticker:
+                        <input
+                            type="text"
+                            name="ticker"
+                            value={items.ticker || ""}
+                            onChange={handleChange}
+                        />
+                    </label>
+                    <label>Enter quantity:
+                        <input
+                            type="number"
+                            name="quantity"
+                            value={items.quantity || ""}
+                            onChange={handleChange}
+                        />
+                    </label>
+                    <input type="submit" />
+                    <p id="log"></p>
+                </div>
             </form>
         </div>
     );
